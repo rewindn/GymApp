0e1ba72002d29effec0833405f3de61f20dbe2b3
Faris Rasyid Raharjo <farismudji@gmail.com>
2026-04-07 16:52:32 +0700

    Membuat Simple Nav Gutton

--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -7,16 +7,36 @@ import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +66,42 @@ fun DoneText( modifier: Modifier = Modifier) {
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationButton(){
+
+    OutlinedButton(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
+        border = BorderStroke(6.dp, Color.Gray)
+
+
+
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.Info,
+            tint = Color.White,
+            contentDescription = "Info",
+            modifier = Modifier.size(24.dp)
+        )
+            Spacer(Modifier.size(53.dp))
+        Icon(
+            imageVector = Icons.Filled.PlayArrow,
+            tint = Color.White,
+            contentDescription = "play",
+            modifier = Modifier.size(46.dp)
+
+        )
+        Spacer(Modifier.size(53.dp,))
+        Icon(
+            imageVector = Icons.Outlined.Edit,
+            tint = Color.White,
+            contentDescription = "edit",
+            modifier = Modifier.size(24.dp)
+        )
+}
+}
+
 @Composable
 fun ImGoodNowButton(){
     Button(
@@ -68,6 +124,7 @@ fun ImGoodNowButton(){
 @Composable
 fun AfterSessionCardPreview() {
     GymAppTheme {
-        ImGoodNowButton()
+
+        NavigationButton()
     }
 }
